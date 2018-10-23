6b141a92b89dab417adc5d96c3d30c9d460aaf8c
lzp48947 <lizhaopeng.cn@gmail.com>
2018-10-23 20:24:50 +0800

    git branch dagger2

---
+++ ARouter/src/main/java/com/lzp/arouter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lzp.arouter;
+
+import com.lzp.basemodule.base.BaseActivity;
+
+/**
+ * Created by lzp48947 on 2018/10/22.
+ */
+
+public class MainActivity extends BaseActivity {
+    @Override
+    public int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
